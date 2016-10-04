12cff40f138884d561f5a5066b033d9a6ef50757
smoyen <smoyen1@gmail.com>
2016-10-03 22:37:49 -0500

    simplify site.
    strip down to just html and css. update hero image. update site theme.

--- DataEntities/User.js
+++
@@ -1,8 +0,0 @@
-var User = (function () {
-    function User(firstName, lastName) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-    }
-    return User;
-}());
-//# sourceMappingURL=User.js.map
--- DataEntities/User.ts
+++
@@ -1,8 +0,0 @@
-class User 
-{
-    constructor (public firstName: string, public lastName: string, private admin: boolean, private authenticated: boolean)
-    {
-    }
-    
-}
-
